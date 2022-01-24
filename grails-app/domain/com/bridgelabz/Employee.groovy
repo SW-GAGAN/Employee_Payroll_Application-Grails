937e7dd9c529893f1adf084f488f1cc842ead482
SW-GAGAN <srgaganreddy0@gmail.com>
2022-01-24 22:39:06 +0530

    Added: Domain class Employee

--- grails-app/domain/com/bridgelabz/Employee.groovy
+++ grails-app/domain/com/bridgelabz/Employee.groovy
@@ -1,7 +1,27 @@
 package com.bridgelabz
 
-class Employee {
+import grails.rest.Resource
+import java.time.LocalDate
+
+@Resource(uri="/employee")
+class EmployeeModel {
+    Long id
+    String name
+    String profilePic
+    String gender
+    String[] department
+    Integer salary
+    LocalDate startDate
+    String note
 
     static constraints = {
+        id unique: true, blank: false
+        name size: 4..20, blank: false
+        profilePic nullable: true
+        gender nullable: true
+        department nullable: true
+        salary nullable: true, min: 10000
+        startDate nullable: true
+        note blank: false
     }
 }
